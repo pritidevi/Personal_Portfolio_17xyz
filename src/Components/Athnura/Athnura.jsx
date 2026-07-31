077a9b2353aa5fe1b2578d9b80155b7d0527e630
priti devi <priti827800@gmail.com>
2026-07-31 16:35:53 +0530

    fix all bugs

--- src/Components/Athnura/Athnura.jsx
+++ src/Components/Athnura/Athnura.jsx
@@ -8,7 +8,7 @@ export default function Athnura() {
         <p className='text-[#7c6e66]'>
            Frontend Developer Intern with 1 month of hands-on experience specializing in building responsive and user-friendly web interfaces using React, JavaScript, HTML, and CSS. Adept at translating UI/UX designs into clean, maintainable code and optimizing web performance for seamless user experiences.
         </p>
-        <button className='bg-[#556e41] flex justify-center w-[138px] h-auto p-[7px] items-center  m-auto rounded-3xl text-white hover:bg-[#435037] cursor-pointer'onClick={()=>window.open('https://www.athenura.in')}>
+        <button className='bg-[#556e41] flex justify-center w-[138px] h-auto p-[7px] my-2 items-center  m-auto rounded-3xl text-white hover:bg-[#435037] cursor-pointer'onClick={()=>window.open('https://www.athenura.in')}>
 VISIT ATHNURA
         </button>
       </div>
